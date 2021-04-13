d0d4ad35dd23f4a4682609137d8a2e1edd5cb4a8
RoussKS <7203382+RoussKS@users.noreply.github.com>
2021-04-13 07:49:30 +0100

    uuid on user

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
+/**
+ * Class User
+ *
+ * @package App\Models
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $name
+ * @property string $email
+ * @property \Carbon\Carbon $email_verified_at
+ * @property string $password
+ * @property string $remember_token
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ *
+ * @property-read \App\Models\ShoppingList $shoppingList
+ */
 class User extends Authenticatable
 {
     use HasFactory;
@@ -40,4 +60,28 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne|\App\Models\ShoppingList
+     */
+    public function shoppingList()
+    {
+        return $this->hasOne(ShoppingList::class);
+    }
+
+    /**
+     * Boot function from Laravel.
+     *
+     * Add UUID to the model.
+     */
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (ShoppingList $shoppingList) {
+            if (empty($shoppingList->uuid)) {
+                $shoppingList->uuid = Str::uuid()->toString();
+            }
+        });
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,6 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Class CreateUsersTable
+ */
 class CreateUsersTable extends Migration
 {
     /**
@@ -15,6 +18,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
