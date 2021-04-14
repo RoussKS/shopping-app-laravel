c48a9c465afb702c998bbcaf4be088fca22e9fa9
RoussKS <7203382+RoussKS@users.noreply.github.com>
2021-04-14 07:33:26 +0100

    add ShoppingList Policy (if user can create or view a list)

---
+++ app/Policies/ShoppingListPolicy.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\ShoppingList;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+/**
+ * Class ShoppingListPolicy
+ *
+ * @package App\Policies
+ */
+class ShoppingListPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any models.
+     *
+     * @param  \App\Models\User  $user
+     *
+     * @return bool
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\ShoppingList  $shoppingList
+     *
+     * @return bool
+     */
+    public function view(User $user, ShoppingList $shoppingList): bool
+    {
+        return $user->id === $shoppingList->user->id;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     * Can only create/have 1 list each time.
+     *
+     * @param  \App\Models\User  $user
+     *
+     * @return bool
+     */
+    public function create(User $user): bool
+    {
+        return $user->shoppingList === null;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\ShoppingList  $shoppingList
+     *
+     * @return bool
+     */
+    public function update(User $user, ShoppingList $shoppingList): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\ShoppingList  $shoppingList
+     *
+     * @return bool
+     */
+    public function delete(User $user, ShoppingList $shoppingList): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\ShoppingList  $shoppingList
+     *
+     * @return bool
+     */
+    public function restore(User $user, ShoppingList $shoppingList): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\ShoppingList  $shoppingList
+     *
+     * @return bool
+     */
+    public function forceDelete(User $user, ShoppingList $shoppingList): bool
+    {
+        return false;
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,10 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
+use App\Models\ShoppingList;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Gate;
+use App\Policies\ShoppingListPolicy;
 
+/**
+ * Class AuthServiceProvider
+ *
+ * @package App\Providers
+ */
 class AuthServiceProvider extends ServiceProvider
 {
     /**
@@ -13,7 +21,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        ShoppingList::class => ShoppingListPolicy::class
     ];
 
     /**
@@ -24,7 +32,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
-        //
     }
 }
